bf25fcd3bf8da575c47f2867660da6068371174d
CdnTiger <73088522+CdnTiger@users.noreply.github.com>
2020-10-21 21:55:12 -0500

    Add files via upload

---
+++ playtStatic.js
@@ -0,0 +1,81 @@
+var playt = {};
+
+(function() {
+
+    playt.cards = [
+        {   name: "playing_cards",
+            include: 1,
+            cardBack: "cards/playing_cards.jpg",
+            images: [
+                "cards/playing_cards/Expansion1.png",
+                "cards/playing_cards/Expansion2.png",
+                "cards/playing_cards/Expansion3.png",
+                "cards/playing_cards/Expansion4.png",
+                "cards/playing_cards/Expansion5.png",
+                "cards/playing_cards/Expansion6.png",
+                "cards/playing_cards/Expansion7.png",
+                "cards/playing_cards/Expansion8.png",
+                "cards/playing_cards/Expansion9.png",
+                "cards/playing_cards/Expansion10.png",
+                "cards/playing_cards/Expansion11.png",
+                "cards/playing_cards/Expansion12.png",
+                "cards/playing_cards/Expansion13.png",
+                "cards/playing_cards/Reverberation1.png",
+                "cards/playing_cards/Reverberation2.png",
+                "cards/playing_cards/Reverberation3.png",
+                "cards/playing_cards/Reverberation4.png",
+                "cards/playing_cards/Reverberation5.png",
+                "cards/playing_cards/Reverberation6.png",
+                "cards/playing_cards/Reverberation7.png",
+                "cards/playing_cards/Reverberation8.png",
+                "cards/playing_cards/Reverberation9.png",
+                "cards/playing_cards/Reverberation10.png",
+                "cards/playing_cards/Reverberation11.png",
+                "cards/playing_cards/Reverberation12.png",
+                "cards/playing_cards/Reverberation13.png",
+                "cards/playing_cards/Solar-Flare1.png",
+                "cards/playing_cards/Solar-Flare2.png",
+                "cards/playing_cards/Solar-Flare3.png",
+                "cards/playing_cards/Solar-Flare4.png",
+                "cards/playing_cards/Solar-Flare5.png",
+                "cards/playing_cards/Solar-Flare6.png",
+                "cards/playing_cards/Solar-Flare7.png",
+                "cards/playing_cards/Solar-Flare8.png",
+                "cards/playing_cards/Solar-Flare9.png",
+                "cards/playing_cards/Solar-Flare10.png",
+                "cards/playing_cards/Solar-Flare11.png",
+                "cards/playing_cards/Solar-Flare12.png",
+                "cards/playing_cards/Solar-Flare13.png",
+                "cards/playing_cards/Trigger1.png",
+                "cards/playing_cards/Trigger2.png",
+                "cards/playing_cards/Trigger3.png",
+                "cards/playing_cards/Trigger4.png",
+                ]
+        }
+    ];
+    
+    playt.dice = [
+        {   name: "d6",
+            include: 5,
+            images: [
+                "dice/d6/dice-1-th.png",
+                "dice/d6/dice-2-th.png",
+                "dice/d6/dice-3-th.png",
+                "dice/d6/dice-4-th.png",
+                "dice/d6/dice-5-th.png",
+                "dice/d6/dice-6-th.png"
+            ]
+        },
+        {   name: "cats4",
+            include: 0,
+            images: [
+                "dice/cats4/cat1.png",
+                "dice/cats4/cat2.png",
+                "dice/cats4/cat3.png",
+                "dice/cats4/cat4.png",
+                "dice/cats4/cat5.png",
+                "dice/cats4/cat6.png"
+            ]
+        }
+    ];
+})();
